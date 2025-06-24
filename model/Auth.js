c816c97312454e8211129e963e05061b662ac1aa
shital chatarkar <shitalchatarkar21@gmail.com>
2025-06-24 13:53:58 +0530

    doteadd

--- model/Auth.js
+++ model/Auth.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-module.exports = mongoose.model("user", new mongoose.Schema({
+module.exports = mongoose.model("auth", new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true }
