997e81ffb5b121bbd55c0a630728794e3af6c783
shital chatarkar <shitalchatarkar21@gmail.com>
2025-06-24 13:46:57 +0530

    dotenv add

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express")
-const { default: mongoose } = require("mongoose")
+const mongoose = require("mongoose")
+
+require("dotenv").config()
 const app = express()
+
 app.use(express.json())
+app.use(express.static("dist"))
 
 
 app.use("/api/auth", require("./routes/auth.routes"))
-mongoose.connect("mongodb://localhost:27017/roma")
+mongoose.connect(process.env.MONGODB_URL)
 mongoose.connection.once("open", () => {
     console.log("db connection success")
 
-    app.listen(5000, console.log("server success"))
+    app.listen(process.env.PORT, console.log("server success"))
 })
