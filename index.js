b44b223a89fdb3464259a8f4a6b1b693efad7050
shital chatarkar <shitalchatarkar21@gmail.com>
2025-06-24 16:57:36 +0530

    fend added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 require("dotenv").config()
 const app = express()
@@ -7,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static("dist"))
 
-
+app.use(cors({ origin: "https://auth-practise-3.onrender.com" }))
 app.use("/api/auth", require("./routes/auth.routes"))
 mongoose.connect(process.env.MONGODB_URL)
 mongoose.connection.once("open", () => {
